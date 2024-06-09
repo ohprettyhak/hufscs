73c93a96eb4c2ec66cff05a20c5a41dc9ea39ed2
ohprettyhak <haklee.googl@gmail.com>
2024-06-09 16:04:21 +0900

    chore: OS HW3 파일명 변경

---
+++ Operating System(V41009201-01)/HW3/HW3-git-upload-script.bash
@@ -0,0 +1,11 @@
+# Created on: 06/09/2024
+# Program: Delayed Number Printer
+# Author: Hak Lee
+
+git init
+git add .
+git commit -m "feat: OS HW3 프로그램 파일 추가"
+git branch -M main
+git remote add origin https://github.com/ohprettyhak/hufscs.git
+git push origin -u main
+
