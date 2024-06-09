b47caec968c0ca74f2d2fd7a33505de2ec489784
ohprettyhak <haklee.googl@gmail.com>
2024-06-09 17:36:20 +0900

    feat: OS HW3 pycharm 원격 실행 파이썬 코드 추가

---
+++ Operating System(V41009201-01)/HW3/HW3-pycharm-remote-practice.py
@@ -0,0 +1,13 @@
+"""
+Created on: 06/09/2024
+
+Program: Delayed Alphabet Printer
+Author: Hak Lee
+"""
+
+import time
+
+# A: 65, Z: 90
+for i in range(65, 91):
+    print(chr(i))
+    time.sleep(5)
